a499de13bdfc5abcf2d265b1d41925bc88b88b8d
Harsh Gupta <41621579+harsh-95@users.noreply.github.com>
2018-08-24 16:55:55 +0530

    Update script-jQuery.js
    made common methods for same validations added replace method for spaces in address field

--- JAVASCRIPT and JQUERY/js/script-jQuery.js
+++ JAVASCRIPT and JQUERY/js/script-jQuery.js
@@ -5,26 +5,28 @@ $(document).ready(function() {
 var regNumber = /^[0-9]+$/
 var reg = /^[a-zA-Z]+$/;
 var re = /\S+@\S+\.\S+/;
-var regPass = /[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]*/;
+var regPass = /([A-Za-z]+.*[0-9]|[0-9]+.*[A-Za-z])[A-Za-z0-9]*/;
+var regAddress = /[a-zA-Z]+/
 
 
 //functions to be run on submit button to validate each input field  
 $('#submit').click(function() {
-
-				validateLastName(); validateMiddleName(); validateFirstName();validateEmail();
-			validatePassword();validatePhone();validateDOB(); validateGender(); validateCurrentAdress();
-			validatePermanentAddress(); validateCityName(); validateStateName(); validateZip();
+	
+			validateRequiredName('first'); validateMiddleName(); validateRequiredName('last'); validateEmail();
+			validatePassword();validatePhone();validateDOB(); validateGender(); validateAddress('current');
+			validateAddress('permanent'); validateSelectField('country'); validateSelectField('state'); validateSelectField('city');
   
 					//returns true only when all functions returns true
-			return  (validateFirstName() && validateMiddleName() && validateLastName() && validateEmail() && validatePassword()
-					&& validatePhone() && validateDOB() && validateGender() && validateCurrentAdress() && validatePermanentAddress()
-					&& validateCityName() && validateStateName() && validateZip());
+			return  (validateRequiredName('first') && validateMiddleName() && validateRequiredName('last') && validateEmail() && validatePassword()
+					&& validatePhone() && validateDOB() && validateGender() && validateAddress('current') && validateAddress('permanent')
+					&& validateSelectField('country') && validateSelectField('state') && validateSelectField('city'));    
+				
 })
 
 
 //blur functions for each input field
 $("#first").blur(function(){
-		validateFirstName();
+		validateRequiredName('first');
   });  
   
 $("#middle").blur(function(){
@@ -32,7 +34,7 @@ $("#middle").blur(function(){
   });
   
 $("#last").blur(function(){
-		validateLastName();
+		validateRequiredName('last');
   });
 
 $("#email").blur(function(){
@@ -51,42 +53,47 @@ $("#dob").blur(function(){
 		validateDOB();
   }); 
   
+$(".gender").change(function(){
+		validateGender();
+  });
+  
 $("#current").blur(function(){
-		validateCurrentAdress();
+		validateAddress('current');
   });
   
 $("#permanent").blur(function(){
-		validatePermanentAddress();
+		validateAddress('permanent');
   });
   
-$("#city").blur(function(){
-		validateCityName();
+  
+$("#countryId").change(function(){
+		validateSelectField('country');
   });
   
-$("#state").blur(function(){
-		validateStateName();
+$("#stateId").change(function(){
+		validateSelectField('state');
   });
   
-$("#zip").blur(function(){
-		validateZip();
+$("#cityId").change(function(){
+		validateSelectField('city');
   });
   
-//function to validate first name
-function validateFirstName(){
+//function to validate first and last name
+function validateRequiredName(field){
 	var submit = false;
-	var firstname =$("#first").val();
+	var fieldname =$("#"+field).val();
 	
-		if(firstname ==''){
-			submit = showError('first','Please fill first Name');
+		if(fieldname ==''){
+			submit = showError(field,"Please fill "+field+" name");
 		}
-		else if(!(reg.test(firstname))){
-			submit = showError('first',"Please enter only alphabets in first Name");
+		else if(!(reg.test(fieldname))){
+			submit = showError(field,"Please enter only alphabets in "+field+" name");
 		}
-		else if(firstname.length < 3){
-			submit = showError('first',"First Name must have atleast 3 characters");
+		else if(fieldname.length < 3){
+			submit = showError(field,field+" name must have atleast 3 characters");
 		}
 		else{
-			submit = isFieldCorrect("first");
+			submit = isFieldCorrect(field);
 		}
 		return submit;
 }
@@ -98,32 +105,15 @@ function validateMiddleName(){
 	
 		if(middlename != ''){
 			if(!(reg.test(middlename))){
-				submit = showError('middle',"Please enter only alphabets in Middle Name");
+				submit = showError('middle',"Please enter only alphabets in middle name");
 			}
 			else{
 				submit = isFieldCorrect("middle");
 			}
 		}
-		return submit;
-}
-
-//function to validate last name
-function validateLastName(){
-	var submit = false;
-	var lastname =$("#last").val();
-	
-		if(lastname ==''){
-			submit = showError('last','Please fill Last Name');
-		}
-		else if(!(reg.test(lastname))){
-			submit = showError('last',"Please enter only alphabets in Last Name");
-		}
-		else if(lastname.length < 3){
-			submit = showError('last',"Last Name must have atleast 3 characters");
-		}
 		else{
-			submit = isFieldCorrect("last");
-		}
+				submit = isFieldCorrect("middle");
+			}
 		return submit;
 }
 
@@ -135,11 +125,8 @@ function validateEmail(){
 		if(email ==''){
 			submit = showError('email','Please fill email');
 		}
-		else if(!(re.test(email))){
+		else if((!(re.test(email))) || checkFreq(email) > 1){
 			submit = showError('email',"Please enter email in proper format");
-		}
-		else if(checkFreq(email) > 1){
-			submit = showError('email',"can't have multiple @ symbols");
 		}
 		else{
 			submit = isFieldCorrect("email");
@@ -180,19 +167,13 @@ function validatePassword(){
 	var password = $("#password").val();
 		password = password.trim();
 		if(password ==''){
-			submit = showError('password',"Please fill an alphanumeric Password");
+			submit = showError('password',"Please fill Password");
 		}
 		else if(password.length < 8){
-			submit = showError('password',"Please have min 8 characters in Password without end spaces");
-		}
-		else if((regNumber.test(password))){
-			submit = showError('password',"password can't be only numerics");
-		}
-		else if((reg.test(password))){
-			submit = showError('password',"password can't be only alphabets");
+			submit = showError('password',"Password must be alphanumeric and min 8 charaters");
 		}
 		else if(!(regPass.test(password))){
-			submit = showError('password',"Password should be alphanumeric String");
+			submit = showError('password',"Password must be alphanumeric and min 8 charaters");
 		}
 		else{
 			submit = isFieldCorrect("password");
@@ -231,88 +212,36 @@ function validateGender(){
 		return submit;
 }
 
-//function to validate Current Address
-function validateCurrentAdress(){
+//for validating Address and replaces multple spaces to one space
+function validateAddress(field){
 	var submit = false;
-	var currentAddress = $("#current").val();
-	currentAddress = currentAddress.trim();
-		if(currentAddress ==''){
-			submit = showError('current',"Please fill current Address");
+	var addressField = $("#"+field).val() ;
+	addressField = addressField.trim();
+	
+		if(addressField ==''){
+		submit = showError(field,"Please fill "+field+" address");
+		}
+		else if(!regAddress.test(addressField)){
+			document.getElementById(field).value = addressField.replace(/\s+/g,' ');
+			submit = showError(field,"Please enter a valid "+field+" address");
 		}
 		else{
-			submit = isFieldCorrect("current");
+			document.getElementById(field).value = addressField.replace(/\s+/g,' ');
+			submit = isFieldCorrect(field);
 		}
 		return submit;
 }
 
-//function to validate Permanent Address
-function validatePermanentAddress(){
+//function to validate Select Field
+function validateSelectField(field){
 	var submit = false;
-	var permanentAddress = $("#permanent").val();
-	permanentAddress = permanentAddress.trim();
+	var selectedItem =$("#"+field+'Id').val();
 	
-		if(permanentAddress ==''){
-			submit = showError('permanent',"Please fill permanent Address");
+		if(selectedItem ==''){
+			submit = showError(field+'Id','Please select a '+field);
 		}
 		else{
-			submit = isFieldCorrect("permanent");
-		}
-		return submit;
-}
-
-//function to validate City Name
-function validateCityName(){
-	var submit = false;
-	var city =$("#city").val();
-	
-		if(city ==''){
-			submit = showError('city','Please fill city Name');
-		}
-		else if(!(reg.test(city))){
-			submit = showError('city',"Please enter only alphabets in city Name");
-		}
-		else if(city.length < 3){
-			submit = showError('city',"city must have atleast 3 characters");
-		}
-		else{
-			 submit = isFieldCorrect("city");
-		}
-		return submit;
-}
-
-//function to validate State 
-function validateStateName(){
-	var submit = false;
-	var state =$("#state").val();
-	
-		if(state ==''){
-			submit = showError('state','Please fill state Name');
-		}
-		else if(!(reg.test(state))){
-			submit = showError('state',"Please enter only alphabets in state Name");
-		}
-		else if(state.length < 3){
-			submit = showError('state',"state must have atleast 3 characters");
-		}
-		else{
-			submit = isFieldCorrect("state");
-		}
-		return submit;
-}
-
-//function to validate Zip Code
-function validateZip(){
-	var submit = false;
-	var zip =$("#zip").val();
-	
-		if(zip ==''){
-			submit = showError('zip','Please fill zip Code');
-		}
-		else if(zip.length < 6 || zip.length > 6){
-			submit = showError('zip',"Please enter a valid 6 digit zip code");
-		}
-		else{
-			submit = isFieldCorrect("zip");
+			submit = isFieldCorrect(field+'Id');
 		}
 		return submit;
 }
